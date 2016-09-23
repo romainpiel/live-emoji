65efba59c4ec19e94722c1608c14861968c80801
Romain Piel <romain.piel@songkick.com>
2016-09-22 21:25:40 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Live emojis
+
+Animating Google emojis with Android animated vector drawables.
+
+All credits to [noto-emoji](https://github.com/googlei18n/noto-emoji) for the original emoji.
+
+Feel free to submit a PR with other emoji, it will make me happy.
+
+## License
+```
+Copyright 2016 Romain Piel
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ app/src/main/java/com/romainpiel/liveemoji/MainActivity.java
@@ -0,0 +1,27 @@
+package com.romainpiel.liveemoji;
+
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView u1f603 = (ImageView) findViewById(R.id.u1f603);
+        ImageView u263a = (ImageView) findViewById(R.id.u263a);
+        ImageView u1f635 = (ImageView) findViewById(R.id.u1f635);
+        animate(u1f603, u263a, u1f635);
+    }
+
+    private void animate(ImageView... imageViews) {
+        for (ImageView imageView : imageViews) {
+            AnimatedVectorDrawable drawable = (AnimatedVectorDrawable) imageView.getDrawable();
+            drawable.start();
+        }
+    }
+}
